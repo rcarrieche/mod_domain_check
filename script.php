d66ba7ec01492fadd06c8d0ad1da7302cd77da7e
Renato Correa <rca.zero@gmail.com>
2013-09-08 13:05:40 -0300

    Ajuste módulo

--- script.php
+++ script.php
@@ -16,7 +16,7 @@ class mod_domainCheckInstallerScript {
          $db = JFactory::getDBO();
          $query = $db->getQuery(true);
          $query->update($db->quoteName('#__extensions'));
-         $defaults = '{"moduleclass_sfx":"","pretext":"","label":"Nome do dom\u00ednio","buttontext":"Verificar Dom\u00ednio","checkall":"Marcar Todos","resulttext":"Resultados da busca","forwardurl":"","avaliable_text":"Dispon\u00edvel","taken_text":"Indispon\u00edvel","tlds":["com","net","org","gov","info"],"countries":["iii","br"]}';
+         $defaults = '{"moduleclass_sfx":"lalala","pretext":"kkkkkk","label":"Nome do dom\u00ednio","buttontext":"Verificar Dom\u00ednio","checkall":"Marcar Todos","resulttext":"Resultados da busca","forwardurl":"","avaliable_text":"Dispon\u00edvel","taken_text":"Indispon\u00edvel","tlds":["com","net","org","gov","info"],"countries":["iii","br"]}';
          $query->set($db->quoteName('params') . ' = ' . $db->quote($defaults));
          $query->where($db->quoteName('name') . ' = ' . $db->quote('mod_domain_check')); // com_XXX is your component 
          $db->setQuery($query);
